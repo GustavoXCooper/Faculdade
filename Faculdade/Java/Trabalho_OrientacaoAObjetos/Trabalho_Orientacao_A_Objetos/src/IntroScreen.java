99abeb70b30a1c6cff29e39394d9186ffa31ed17
Gustavo Cooper <gustavoxcooper@gmail.com>
2023-05-26 13:44:26 -0300

    Update IntroScreen.java

--- Faculdade/Java/Trabalho_OrientacaoAObjetos/Trabalho_Orientacao_A_Objetos/src/IntroScreen.java
+++ Faculdade/Java/Trabalho_OrientacaoAObjetos/Trabalho_Orientacao_A_Objetos/src/IntroScreen.java
@@ -1,6 +1,6 @@
 public class IntroScreen {
 	/* This was for my professor only, I kept my name and all the other information except for the RA (Used to login and other stuff  
-	on my university).
+	on my university website).
 	Also, I informed the login and password so anyone can access and test the program without looking at the code
 	*/
 	public void Intro() {
