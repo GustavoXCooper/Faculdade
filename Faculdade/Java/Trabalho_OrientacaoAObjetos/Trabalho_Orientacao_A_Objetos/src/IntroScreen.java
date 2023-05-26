de3c490958084e8c43e43c02e9321881a73b6c47
Gustavo Cooper <gustavoxcooper@gmail.com>
2023-05-26 13:44:04 -0300

    Update IntroScreen.java

--- Faculdade/Java/Trabalho_OrientacaoAObjetos/Trabalho_Orientacao_A_Objetos/src/IntroScreen.java
+++ Faculdade/Java/Trabalho_OrientacaoAObjetos/Trabalho_Orientacao_A_Objetos/src/IntroScreen.java
@@ -1,8 +1,12 @@
 public class IntroScreen {
+	/* This was for my professor only, I kept my name and all the other information except for the RA (Used to login and other stuff  
+	on my university).
+	Also, I informed the login and password so anyone can access and test the program without looking at the code
+	*/
 	public void Intro() {
 		System.out.println("Bem vindo!\n\nEsse trabalho foi realizado para a extensao Orientacao a Objetos com Java");
 		System.out.println("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-		System.out.println("\nNome: Gustavo Xavier Cooper\nRa: 1292313178\nUnidade: Uniritter, FAPA\nIDE: Ecplise\n");	
+		System.out.println("\nNome: Gustavo Xavier Cooper\nRa: 0000000\nUnidade: Uniritter, FAPA\nIDE: Ecplise\n");	
 		System.out.println("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
 		System.out.println("\nSeu login:RubemMKoide\nSua senha:123\n\n");	
 		System.out.println("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
